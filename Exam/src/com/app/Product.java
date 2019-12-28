9b6357514bf2285f7c45e135c31d5cfb9ca77ebe
sameer17github <group009@infoway.com>
2019-12-28 15:22:49 +0530

    first commit

---
+++ Exam/src/com/app/Product.java
@@ -0,0 +1,35 @@
+package com.app;
+
+public class Product 
+{
+	String productName,ProductD;
+	int Id,ProductQuantity;
+	
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductD() {
+		return ProductD;
+	}
+	public void setProductD(String productD) {
+		ProductD = productD;
+	}
+	public int getProductQuantity() {
+		return ProductQuantity;
+	}
+	public void setProductQuantity(int productQuantity) {
+		ProductQuantity = productQuantity;
+	}
+
+
+}
